d1136b6f63cb6aa5c5c903d3a5397df2673dbc29
danielfabricio <danielfabricio@alunos.utfpr.edu.br>
2026-05-07 13:00:32 -0300

    up

--- process.py
+++ process.py
@@ -4,14 +4,12 @@ import threading
 import time, random
 from datetime import datetime
 
-
 list_uri = [
     "PYRO:process0@localhost:9090",
     "PYRO:process1@localhost:9091",
     "PYRO:process2@localhost:9092",
     "PYRO:process3@localhost:9093",
 ]
-
 
 class Raft():
     def __init__(self, id):
@@ -27,17 +25,18 @@ class Raft():
         self._id_last_leader = 0
         self._term = 1
 
+    def get_current_time(self):
+        return datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    
     @Pyro5.api.expose
     def request_vote(self, id_candidate):
         if self._voter_for is None:
             self._voter_for = id_candidate
 
-            hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-            print(f' | {hora} | Process {self._id} votou em {id_candidate}')
+            print(f' | {self.get_current_time()} | Process {self._id} votou em {id_candidate}')
             return 1
         
         return 0
-
 
     @Pyro5.api.expose
     def heartbeat(self, leader_id):
@@ -47,11 +46,9 @@ class Raft():
             self._id_last_leader = leader_id
             self._term += 1
             self._voter_for = None
-            hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-            print(f'| {hora} | Process {self._id} NOVO LIDER DETECTADO {leader_id}')
+            print(f'| {self.get_current_time()} | Process {self._id} NOVO LIDER DETECTADO {leader_id}')
 
-        hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-        print(f'| {hora} | Process {self._id} recebeu heartbeat do lider {leader_id}')
+        print(f'| {self.get_current_time()} | Process {self._id} recebeu heartbeat do lider {leader_id}')
 
 
     @Pyro5.api.expose
@@ -62,8 +59,7 @@ class Raft():
         self._isleader = True
         self._votes = 0
 
-        hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-        print(f' | {hora} | Process {self._id} virou LIDER')
+        print(f' | {self.get_current_time()} | Process {self._id} virou LIDER')
 
         threading.Thread(
             target=self.run_leader,
@@ -74,8 +70,7 @@ class Raft():
         self._isleader = False
         self._votes = 0
 
-        hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-        print(f' | {hora} | Process {self._id} virou SEGUIDOR')
+        print(f' | {self.get_current_time()} | Process {self._id} virou SEGUIDOR')
 
         threading.Thread(
             target=self.run_follower,
@@ -87,20 +82,25 @@ class Raft():
         start = time.time()
 
         while self._running:
-            if time.time() - start >20:
+            if time.time() - start > 20:
                 self._running = False
                 self._isleader = False
 
-                hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                print(f'| {hora} | LIDER CAIU {self._id}')
+                print(f'| {self.get_current_time()} | LIDER CAIU {self._id}')
                 break
 
             for i, process_uri in enumerate(list_uri):
                 if i == self._id:
                     continue
+                
+                try:
+                    process_proxy = Pyro5.api.Proxy(process_uri) 
+                    # process_proxy._pyroBind()
+                    process_proxy.heartbeat(self._id)
 
-                process_proxy = Pyro5.api.Proxy(process_uri) 
-                process_proxy.heartbeat(self._id)
+                except:
+                    print('não está online')
+                    pass
 
             time.sleep(0.500)
 
@@ -109,17 +109,20 @@ class Raft():
             elapsed = time.time() - self._last_heartbeat
 
             if elapsed > self._time_out:
-                hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                print(f'| {hora} | Process {self._id} virou CANDIDATO ')
+                print(f'| {self.get_current_time()} | Process {self._id} virou CANDIDATO ')
                 self._votes = 1
                 self._voter_for = self._id
 
                 for i, process_uri in enumerate(list_uri):
                     if i == self._id:
                         continue
-
-                    process_proxy = Pyro5.api.Proxy(process_uri)
-                    self._votes += process_proxy.request_vote(self._id)
+                    
+                    try:
+                        process_proxy = Pyro5.api.Proxy(process_uri)
+                        self._votes += process_proxy.request_vote(self._id)
+                    except:
+                        print('não está online')
+                        pass
 
                 if self._votes >= 2:
                     self._isleader = True
@@ -127,8 +130,7 @@ class Raft():
                     self._voter_for = None
                     self._term += 1
 
-                    hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                    print(f'| {hora} | Process {self._id} VIROU LIDER')
+                    print(f'| {self.get_current_time()} | Process {self._id} VIROU LIDER')
                     self.became_leader()
                     break
 
@@ -138,8 +140,7 @@ class Raft():
                     self._last_heartbeat = time.time()
                     self._time_out = random.uniform(0.500, 0.750)
 
-                    hora = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                    print(f' | {hora} | Process {self._id} PERDER ELEIÇÃO')
+                    print(f' | {self.get_current_time()} | Process {self._id} PERDEU ELEIÇÃO')
 
 
 n_process = int(input("nº do processo: "))
@@ -149,16 +150,8 @@ objeto_process = Raft(n_process)
 
 uri = daemon.register(objeto_process, objectId=f"process{n_process}")
 
-
 print('uri: ', uri)
 
-if n_process > 2:
-    print('Processo iniciado \n\n')
-    objeto_process.became_leader()
 
-else:
-    print('Processo iniciado \n\n')
-    objeto_process.became_follower()
-
-
+objeto_process.became_follower()
 daemon.requestLoop()
